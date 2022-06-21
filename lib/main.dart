229f5e73f8010ed4a11f3f52d521b21d25789f10
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-06-21 12:27:14 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CLIMA',
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const LoadingScreen(),
+    );
+  }
+}
